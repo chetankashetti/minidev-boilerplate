caf07dc65b953645150bc27507f5dba14630124d
Chetan <chetankashetty@gmail.com>
2025-10-15 17:19:18 +0530

    refactor: simplify wagmi configuration by removing base chain and retaining only baseSepolia

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,12 +1,11 @@
-import { base, baseSepolia } from "wagmi/chains";
+import { baseSepolia } from "wagmi/chains";
 import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
 import { createConfig, http } from "wagmi";
 
 export const config = createConfig({
-  chains: [baseSepolia, base],
+  chains: [baseSepolia],
   transports: {
     [baseSepolia.id]: http(),
-    [base.id]: http(),
   },
   connectors: [farcasterMiniApp()],
   ssr: true,
